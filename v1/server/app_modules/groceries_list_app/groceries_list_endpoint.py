170fddcabf011e49a73d4420543392fb1abd6712
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-22 22:11:35 +0100

    Add grocery lists GET initial test.

--- v1/server/app_modules/groceries_list_app/groceries_list_endpoint.py
+++ v1/server/app_modules/groceries_list_app/groceries_list_endpoint.py
@@ -2,8 +2,19 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf-8 :
 # author: 'ACIOBANI'
+from datetime import datetime
+from uuid import NAMESPACE_DNS, uuid5
+
+from flask import make_response, jsonify
 from flask.views import MethodView
 
 
 class GroceriesListEndpoint(MethodView):
-    pass
+    def get(self):
+        name = 'Grocery List 1'
+        get_groceries_list = {'name': name,
+                              'id': uuid5(NAMESPACE_DNS, name),
+                              'total_items': 10,
+                              'created_at': datetime.now(),
+                              'created_by': 'me as string for now'}
+        return make_response(jsonify(get_groceries_list)), 200
