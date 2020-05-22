6618edb07085c4f2a6533ad12a5dd0f0e37eec0c
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-22 22:19:57 +0100

    Register groceries list blueprint, and fix errors.

--- v1/server/app_modules/groceries_list_app/views.py
+++ v1/server/app_modules/groceries_list_app/views.py
@@ -10,8 +10,8 @@ from flask_jwt_auth.v1.server.app_modules.groceries_list_app.groceries_list_endp
 groceries_list_bp = Blueprint('groceries_list', __name__)
 
 # define auth API resources
-groceries_list_bp = GroceriesListEndpoint.as_view('register_api')
+groceries_list_view = GroceriesListEndpoint.as_view('groceries_list_api')
 
 groceries_list_bp.add_url_rule('/groceries',
-                               view_func=groceries_list_bp,
+                               view_func=groceries_list_view,
                                methods=['GET'])
