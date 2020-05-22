44ad3ee0f6381982c25b6a462d7033341f364601
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-22 22:11:53 +0100

    Add groceries_list_tests.

---
+++ v1/tests/groceries_list_ep_tests/test_groceries_list_ep.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+# author: 'ACIOBANI'
+import json
+import unittest
+from time import sleep
+
+from flask_jwt_auth.v1.server import db_sql
+from flask_jwt_auth.v1.server.models import User, BlacklistToken
+from flask_jwt_auth.v1.tests.base import BaseTestCase
+
+
+class TestAuthBlueprint(BaseTestCase):
+    auth = None
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_can_get_a_grocery_list(self):
+        """
+        Test can successfully get a grocery list.
+        """
+        with self.client:
+            data = self.client.get('/groceries',
+                                   data='',
+                                   content_type='application/json')
+            print('test_can_get_a_grocery_list:: \n', data)
+
+            self.assertTrue(data)
+            # self.assertTrue(data['status'] == 'success')
+            # self.assertTrue(data['message'] == 'Successfully registered.')
+            # self.assertTrue(data['auth_token'])
+            # self.assertTrue(data['content_type'] == 'application/json')
+            # self.assertEqual(data['status_code'], 201)
