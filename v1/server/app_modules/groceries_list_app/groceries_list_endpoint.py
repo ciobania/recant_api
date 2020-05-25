95a1e5df3ac469d8b48619c4e7b625bc840accab
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-25 09:04:51 +0100

    Refactor groceries list endpoint, and auth validation, error messages.

--- v1/server/app_modules/groceries_list_app/groceries_list_endpoint.py
+++ v1/server/app_modules/groceries_list_app/groceries_list_endpoint.py
@@ -5,16 +5,23 @@
 from datetime import datetime
 from uuid import NAMESPACE_DNS, uuid5
 
-from flask import make_response, jsonify
+from flask import make_response, jsonify, g
 from flask.views import MethodView
+
+from flask_jwt_auth.v1.server.auth.auth_helpers import login_required
 
 
 class GroceriesListEndpoint(MethodView):
+    @login_required
     def get(self):
+        """
+        Scaffolding for GET HTTP request: /groceries/
+        :return: JSON API response
+        """
         name = 'Grocery List 1'
         get_groceries_list = {'name': name,
                               'id': uuid5(NAMESPACE_DNS, name),
                               'total_items': 10,
                               'created_at': datetime.now(),
-                              'created_by': 'me as string for now'}
+                              'created_by': g.user.id}
         return make_response(jsonify(get_groceries_list)), 200
--- v1/tests/groceries_list_ep_tests/test_groceries_list_ep.py
+++ v1/tests/groceries_list_ep_tests/test_groceries_list_ep.py
@@ -3,19 +3,20 @@
 # vim: set fileencoding=utf-8 :
 # author: 'ACIOBANI'
 import json
-import unittest
-from time import sleep
+from uuid import UUID
 
-from flask_jwt_auth.v1.server import db_sql
-from flask_jwt_auth.v1.server.models import User, BlacklistToken
+from flask import g
+
+from flask_jwt_auth.v1.server.models import User
 from flask_jwt_auth.v1.tests.base import BaseTestCase
+from flask_jwt_auth.v1.tests.user_auth_ep_tests.auth_helpers import AuthHelpers
 
 
-class TestAuthBlueprint(BaseTestCase):
+class TestGroceriesBlueprint(BaseTestCase):
     auth = None
 
     def setUp(self):
-        pass
+        self.auth = AuthHelpers(self.client)
 
     def tearDown(self):
         pass
@@ -24,15 +25,39 @@ class TestAuthBlueprint(BaseTestCase):
         """
         Test can successfully get a grocery list.
         """
+        user_payload = {'email': 'grocery_list_get@mailinator.com',
+                        'password': '1234567890'}
+        with self.client:
+            data = self.auth.register_user(user_payload=user_payload)
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully registered.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(data['content_type'] == 'application/json')
+            self.assertEqual(data['status_code'], 201)
+
+            data = self.auth.login_user(user_payload)
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully logged in.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(data['content_type'] == 'application/json')
+            self.assertEqual(data['status_code'], 200)
+
+            data = self.auth._request('/groceries', 'get', auth_token=data['auth_token'])
+            self.assertTrue(data['name'] == 'Grocery List 1')
+            self.assertTrue(isinstance(data['id'], str))
+            self.assertTrue(data['total_items'] == 10)
+            print('grocery_list_data', data, '\n')
+            print('g.user is::', g.user)
+
+    def test_cannot_get_grocery_list_without_login(self):
+        """
+        Test grocery list endpoint is not accessible without login.
+        """
         with self.client:
             response = self.client.get('/groceries',
                                        data='',
                                        content_type='application/json')
-            print('test_can_get_a_grocery_list:: \n', response.data.decode())
-
-            self.assertTrue(response)
-            # self.assertTrue(data['status'] == 'success')
-            # self.assertTrue(data['message'] == 'Successfully registered.')
-            # self.assertTrue(data['auth_token'])
-            # self.assertTrue(data['content_type'] == 'application/json')
-            # self.assertEqual(data['status_code'], 201)
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'Provide a valid auth token.')
+            self.assertEqual(response.status_code, 403)
