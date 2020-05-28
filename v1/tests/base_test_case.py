05abad7e09bffbc893c37ef9f8d2abdea8cf6a19
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-28 10:50:42 +0100

    Add missing Base test case.

---
+++ v1/tests/base_test_case.py
@@ -0,0 +1,25 @@
+# project/server/tests/base.py
+
+
+from flask_testing import TestCase
+
+from flask_jwt_auth.v1.server import app, db_sql
+
+
+class BaseTestCase(TestCase):
+    """
+    Base Tests
+    """
+
+    def create_app(self):
+        app.config.from_object('flask_jwt_auth.v1.server.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db_sql.create_all()
+        db_sql.session.commit()
+
+    def tearDown(self):
+        db_sql.session.remove()
+        db_sql.drop_all()
+        db_sql.session.commit()
