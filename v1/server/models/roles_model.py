be99a27b6ad74ea855941a3141cd1e87beb1943b
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-31 18:03:11 +0100

    Add UserRoles association table constraint.

--- v1/server/models/roles_model.py
+++ v1/server/models/roles_model.py
@@ -5,15 +5,17 @@
 from uuid import uuid4
 
 from flask_security import RoleMixin
+from sqlalchemy import UniqueConstraint
 
 from flask_jwt_auth.v1.server import db_sql
 from flask_jwt_auth.v1.server.models import BaseModel
 
 user_roles = db_sql.Table('user_roles', BaseModel.metadata,
                           db_sql.Column('user_id', db_sql.ForeignKey('users.id'), default=uuid4,
-                                        nullable=False, unique=True),
+                                        nullable=False, unique=False),
                           db_sql.Column('role_id', db_sql.ForeignKey('roles.id'),  default=uuid4,
-                                        nullable=False, unique=True))
+                                        nullable=False, unique=False),
+                          UniqueConstraint('user_id', 'role_id', name='user_roles_idx'))
 
 
 class Role(BaseModel, RoleMixin):
