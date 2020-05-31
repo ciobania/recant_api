82436fec718d392f00f6610f70445450fb5596e0
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-31 18:02:24 +0100

    Refactor grocery lists endpoints ou in helpers.

---
+++ v1/tests/groceries_list_ep_tests/helpers.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+# author: 'ACIOBANI'
+from flask_jwt_auth.v1.tests.helpers import RequestHelpers
+
+
+class GroceriesHelpers:
+    def __init__(self, client, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client = client
+        self.rh = RequestHelpers(self.client)
+
+    def share_grocery_list(self, payload, auth_token):
+        data = self.rh.request('/api/groceries/share',
+                               'post',
+                               auth_token=auth_token,
+                               payload=payload)
+
+        return data
+
+    def remove_share_grocery_list(self, payload, auth_token):
+        data = self.rh.request('/api/groceries/share',
+                               'delete',
+                               auth_token=auth_token,
+                               payload=payload)
+
+        return data
+
+    def get_all_grocery_lists(self, auth_token):
+        data = self.rh.request('/api/groceries',
+                               'get',
+                               auth_token=auth_token)
+        return data
+
+    def create_grocery_list(self, payload, auth_token):
+        data = self.rh.request('/api/groceries',
+                               'post',
+                               auth_token=auth_token,
+                               payload=payload)
+
+        return data
+
+    def update_grocery_list(self, payload, auth_token, gls_id):
+        data = self.rh.request('/api/groceries/{}'.format(gls_id),
+                               'post',
+                               auth_token=auth_token,
+                               payload=payload)
+
+        return data
+
+    def delete_grocery_list(self, auth_token, gls_id):
+        data = self.rh.request('/api/groceries/{}'.format(gls_id),
+                               'delete',
+                               auth_token=auth_token)
+
+        return data
