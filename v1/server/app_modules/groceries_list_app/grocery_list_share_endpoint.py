c7b4e326db7fd04244436941a2fe0dd5734af7de
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-31 17:57:59 +0100

    Add groceries share endpoint.

---
+++ v1/server/app_modules/groceries_list_app/grocery_list_share_endpoint.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+# author: 'ACIOBANI'
+from datetime import datetime
+from uuid import NAMESPACE_DNS, uuid5
+
+from flask import make_response, jsonify, g, request
+from flask.views import MethodView
+
+from flask_jwt_auth.v1.server import User
+from flask_jwt_auth.v1.server.auth.auth_helpers import login_required
+from flask_jwt_auth.v1.server.models import GroceriesList
+from flask_jwt_auth.v1.server.models.groceries_list_model import SharedGroceriesList
+
+
+class GroceryListShareEndpoint(MethodView):
+    @login_required
+    def post(self):
+        """
+        Scaffolding for POST HTTP request: /groceries/share
+        :return: JSON API response
+        """
+        request_payload = request.get_json()
+        email = request_payload.get('email')
+        grocery_list_id = request_payload.get('grocery_list_id')
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            # TODO: throw error
+            pass
+        else:
+            grocery_list = GroceriesList.query.filter_by(id=grocery_list_id).first()
+            sgl = SharedGroceriesList(user_id=user.id, grocery_list_id=grocery_list.id, owner=False)
+            sgl.save()
+            response_message = f'Grocery List {grocery_list_id} was shared with user {email}.'
+            response_object = {'status': 'success',
+                               'message': response_message}
+            return make_response(jsonify(response_object)), 200
+
+    @login_required
+    def delete(self):
+        """
+        Scaffolding for DELETE HTTP request: /groceries/share/<uuid:user_id>
+        :return: JSON API response
+        """
+        request_payload = request.get_json()
+        grocery_list_id = request_payload.get('grocery_list_id')
+        email = request_payload.get('email')
+
+        user = SharedGroceriesList.user.has(email=email)
+        filter_gl = GroceriesList.shared_with.any(grocery_list_id=grocery_list_id,
+                                                  owner=False)
+        grocery_list = GroceriesList.query.filter(filter_gl, user)
+        grocery_list.delete(synchronize_session=False)
+        response_object = {'status': 'success',
+                           'message': 'Grocery List with id: {} was un-shared.'.format(grocery_list_id)}
+        return make_response(jsonify(response_object))
--- v1/server/app_modules/groceries_list_app/views.py
+++ v1/server/app_modules/groceries_list_app/views.py
@@ -6,11 +6,13 @@
 from flask import Blueprint
 
 from flask_jwt_auth.v1.server.app_modules.groceries_list_app.groceries_list_endpoint import GroceriesListEndpoint
+from flask_jwt_auth.v1.server.app_modules.groceries_list_app.grocery_list_share_endpoint import GroceryListShareEndpoint
 
 groceries_list_bp = Blueprint('groceries_list', __name__)
 
 # define auth API resources
 groceries_list_view = GroceriesListEndpoint.as_view('groceries_list_api')
+groceries_list_share_view = GroceryListShareEndpoint.as_view('groceries_list_share_api')
 
 groceries_list_bp.add_url_rule('/api/groceries',
                                defaults={'grocery_list_id': None},
@@ -19,3 +21,6 @@ groceries_list_bp.add_url_rule('/api/groceries',
 groceries_list_bp.add_url_rule('/api/groceries/<uuid:grocery_list_id>',
                                view_func=groceries_list_view,
                                methods=['DELETE', 'GET', 'POST'])
+groceries_list_bp.add_url_rule('/api/groceries/share',
+                               view_func=groceries_list_share_view,
+                               methods=['DELETE', 'POST'])
