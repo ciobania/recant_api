2faa8cde2773df264856ed8ad5ce2656b14c2d68
Adrian Ciobanita <grand2pac2000@yahoo.com>
2020-05-31 17:57:02 +0100

    Refactor grocery lists to save to DB.

--- v1/server/app_modules/groceries_list_app/groceries_list_endpoint.py
+++ v1/server/app_modules/groceries_list_app/groceries_list_endpoint.py
@@ -2,22 +2,11 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf-8 :
 # author: 'ACIOBANI'
-from datetime import datetime
-from uuid import NAMESPACE_DNS, uuid5
-
 from flask import make_response, jsonify, g, request
 from flask.views import MethodView
 
 from flask_jwt_auth.v1.server.auth.auth_helpers import login_required
-
-inventory = {
-    1: {'name': "Grocery List 1",
-        'description': 'A first grocery list 1.',
-        'id': uuid5(NAMESPACE_DNS, "Grocery List 1"),
-        'total_items': 10,
-        'created_at': datetime.now(),
-        'created_by': 10003}
-}
+from flask_jwt_auth.v1.server.models import GroceriesList
 
 
 class GroceriesListEndpoint(MethodView):
@@ -27,12 +16,25 @@ class GroceriesListEndpoint(MethodView):
         Scaffolding for GET HTTP request: /groceries/
         :return: JSON API response
         """
+
         if grocery_list_id:
-            grocery_list_obj = tuple(item for item in inventory.values()
-                                     if item['id'] == uuid5(NAMESPACE_DNS, "Grocery List 1"))
-            return make_response(jsonify(grocery_list_obj[0]))
+            try:
+                grocery_list = GroceriesList.query.filter_by(id=grocery_list_id).first()
+                response_object = {'status': 'success',
+                                   'data': grocery_list}
+                return make_response(jsonify(response_object)), 200
+            except Exception as _:
+                err_msg = 'Error occurred: {}. Please try again later.'
+                response_object = {'status': 'fail',
+                                   'message': err_msg.format(_)}
+                return make_response(jsonify(response_object)), 401
         else:
-            return make_response(jsonify(tuple(inventory.values()))), 200
+            filter_gl = GroceriesList.shared_with.any(user_id=g.user.id)
+            groceries_list = GroceriesList.query.filter(filter_gl).order_by(GroceriesList.created_at).all()
+            gls = [item.as_dict() for item in groceries_list if groceries_list]
+            response_object = {'status': 'success',
+                               'data': gls}
+            return make_response(jsonify(response_object)), 200
 
     @login_required
     def post(self, grocery_list_id):
@@ -43,25 +45,26 @@ class GroceriesListEndpoint(MethodView):
         request_payload = request.get_json()
         if not grocery_list_id:
             add_grocery_list = {'name': request_payload.get('name'),
-                                'description': request_payload.get('description'),
-                                'id': uuid5(NAMESPACE_DNS, request_payload.get('name')),
-                                'total_items': 0,
-                                'created_at': datetime.now(),
-                                'created_by': g.user.id}
-            inventory[len(inventory)] = dict(add_grocery_list)
+                                'description': request_payload.get('description')}
+            new_grocery_list = GroceriesList(**add_grocery_list, user=g.user).as_dict()
 
-            return make_response(jsonify(add_grocery_list)), 200
+            response_object = {'status': 'success',
+                               'data': new_grocery_list}
+            return make_response(jsonify(response_object)), 200
         elif grocery_list_id:
+            grocery_list = GroceriesList.query.filter_by(id=grocery_list_id).first()
+            if not grocery_list:
+                response_object = {'status': 'fail',
+                                   'message': 'Resource not found.'}
+                return make_response(jsonify(response_object)), 404
             request_payload = request.get_json()
-            response_object = {'name': request_payload.get('name'),
-                               'description': request_payload.get('description'),
-                               'id': grocery_list_id,
-                               'total_items': 14,
-                               'created_at': datetime.now(),
-                               'created_by': g.user.id}
+            grocery_list.name = request_payload.get('name')
+            grocery_list.description = request_payload.get('description')
+            grocery_list.save()
+
             # if request_payload.keys()
             # validate payload for POST request
-            return make_response(jsonify(response_object))
+            return make_response(jsonify(grocery_list.as_dict())), 200
 
     @login_required
     def delete(self, grocery_list_id):
@@ -73,4 +76,12 @@ class GroceriesListEndpoint(MethodView):
         if grocery_list_id:
             response_object = {'status': 'success',
                                'message': 'Grocery List with id: {} was deleted.'.format(grocery_list_id)}
+            filter_gl = GroceriesList.shared_with.any(user_id=g.user.id, grocery_list_id=grocery_list_id, owner=True)
+            grocery_list = GroceriesList.query.filter(filter_gl)
+            if not grocery_list:
+                response_object = {'status': 'fail',
+                                   'message': 'Resource not found.'}
+                return make_response(jsonify(response_object)), 404
+            else:
+                grocery_list.delete(synchronize_session=False)
             return make_response(jsonify(response_object))
