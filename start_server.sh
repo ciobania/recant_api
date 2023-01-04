9ea935bc7939bfc840541c76e44df8febf090640
ciobania <ciobania>
2023-01-04 19:08:54 +0000

    WIP: Fix merge conflict on start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # service nginx start
-#systemctl start nginx
-#uwsgi --ini configs/uwsgi.ini
+# systemctl start nginx
+# uwsgi --ini configs/uwsgi.ini
+set -o allexport; source v1/.env_test; set +o allexport
+FLASK_APP=manage.py FLASK_DEBUG=1 flask run --host=0.0.0.0 --port=5001
